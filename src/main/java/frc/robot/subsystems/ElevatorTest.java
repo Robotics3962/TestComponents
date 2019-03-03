dbedf0b829a9827218ce00519382fccc5e38facd
Robotics3962 <46796109+Robotics3962@users.noreply.github.com>
2019-03-02 20:07:22 -0500

    filled out elevator test to use encoders and limit switches

--- src/main/java/frc/robot/subsystems/ElevatorTest.java
+++ src/main/java/frc/robot/subsystems/ElevatorTest.java
@@ -23,32 +23,105 @@ public class ElevatorTest extends Subsystem {
   DigitalInput topLimit = null;
   DigitalInput bottomLimit = null;
   Spark motor = null;
+  Encoder encoder = null;
   int count = 0;
+  boolean useLimitSwitches = true;
+  boolean useEncoders = true;
 
   public ElevatorTest(){
     motor = new Spark(RobotMap.SparkElevator);
     motor.enableDeadbandElimination(true);	
+ 
+    if(useLimitSwitches){
+      topLimit = new DigitalInput(RobotMap.ElevatorTopLimitSwitchId);
+      bottomLimit = new DigitalInput(RobotMap.ElevatorBottomLimitSwitchId);
+    }
+
+    if(useEncoders){
+      encoder = new Encoder(RobotMap.ElevatorEncoderPIOId1, RobotMap.ElevatorEncoderPIOId2,false, Encoder.EncodingType.k2X);
+      // initialize encoder
+      encoder.setDistancePerPulse(RobotMap.ElevatorDistPerPulse);
+      encoder.setReverseDirection(RobotMap.ElevatorReverseDirection);
+    
+      // reset needs to be run when the arm is at a known
+      // angle/location,  Preferably this is a physical stop
+      resetEncoder();
+    }
+
+ }
+
+ public void resetEncoder(){
+  if(useEncoders){
+    encoder.reset();
+  }
+}
+
+public boolean atUpperLimit(){
+  if(!useLimitSwitches){
+    return false;
   }
 
-  public void Up(){
-    motor.set(RobotMap.ElevatorUpSpeed);
-    if( (count % 10) == 0) {
-      Robot.encoderTest.LogValues();
-    }
-    count++;
+  return topLimit.get();
+}
 
+public boolean atLowerLimit() {
+  if(!useLimitSwitches){
+    return false;
   }
-  public void Down(){
-    motor.set(RobotMap.ElevatorDownSpeed);
-    if( (count % 10) == 0) {
-      Robot.encoderTest.LogValues();
-    }
-    count++;
+
+  // currently the bottom limit switched is wired
+  // differently so true is not set and false is set
+  // so we need to invert the logic
+  return ! bottomLimit.get();
+}
+
+public void dumpLimitSwitchValues(){
+  if(!useLimitSwitches){
+    return;
   }
-  public void Stop(){
-    motor.stopMotor();
+
+  Robot.Log("Elevator: atLowerLimit:" + atLowerLimit() + " atUpperLimit:" + atUpperLimit());
+}  
+
+// used to manually set the speed which will disable pid control
+public void setSpeed(double speed){
+  motor.set(speed);
+}
+
+public void Up() {
+  //dumpLimitSwitchValues();
+  LogValues();
+  if (atUpperLimit()){
+    Stop();
+  } 
+  else {
+    setSpeed(RobotMap.ElevatorUpSpeed);
   }
-  
+}
+
+public void Stop() {
+  // or call stopmotor()?
+  setSpeed(RobotMap.ElevatorStopSpeed);
+}
+
+public void Down() {
+  LogValues();
+  //dumpLimitSwitchValues();;
+  if (atLowerLimit()){
+    Stop();
+  } 
+  else {
+   setSpeed(RobotMap.ElevatorDownSpeed);
+  }
+}
+
+public void LogValues(){
+  int val = encoder.get();
+  boolean valS = encoder.getStopped();
+  double valR = encoder.getRaw();
+  double valD = encoder.getDistance();
+  Robot.Log("encoder: get:" + val + " stopped:" + valS + " raw:" + valR +  " dist:" + valD);
+}
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
